5f141630b31bece5e2982aa237e488fcd397353d
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-05-02 17:50:33 +0200

    Fix ctags path on Linux

--- test/unit_tests/infrastructure/ctags/ctag_class_mapper_test.py
+++ test/unit_tests/infrastructure/ctags/ctag_class_mapper_test.py
@@ -1,12 +1,19 @@
 import unittest
 import os
+import os.path
 from infrastructure.ctags.ctags_class_mapper import CTagClassMapper, MapperError, CTagLanguage
              
 class CTagClassMapperTest(unittest.TestCase):
+    def get_ctags_exe(self):
+        # TODO this is probably neither the right place to do this, nor the right way
+        if os.path.isfile("/usr/bin/ctags"):
+            return "/usr/bin/ctags"
+        return os.path.join(self.basedir, "../../../../external/ctags/ctags.exe")
+
     def setUp(self):
         #Not sure if the path change is necessary...
         self.basedir = os.path.dirname(__file__)
-        self.mapper = CTagClassMapper(os.path.join(self.basedir, "../../../../external/ctags/ctags.exe"), CTagLanguage.CSharp)
+        self.mapper = CTagClassMapper(self.get_ctags_exe(), CTagLanguage.CSharp)
         fileList = ["TestClass1.cs","TestClass2.cs","TestClassParameter.cs"]
         fileList = [os.path.join(self.basedir,fName) for fName in fileList]
         self.mapper.create_index(fileList)
