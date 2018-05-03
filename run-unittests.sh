8e064f13aa876bcf0439c4013dcb762664976786
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-05-03 10:56:31 +0200

    Ignore some files for test discovery

--- run-unittests.sh
+++ run-unittests.sh
@@ -5,7 +5,7 @@ source $(dirname ${BASH_SOURCE[0]})/src-sh/configuration/config.lib.sh
 {
   # TODO fix setting PYTHONPATH in config.lib.sh, or maybe remove it entirely
   unset PYTHONPATH
-  py.test --doctest-modules --cov=./ --ignore=site-packages
+  py.test --doctest-modules --cov=./ --ignore=site-packages --ignore=pydep_run.py --ignore=independent_part_run.py --ignore=infrastructure/graph_layout/zest --verbose
   exit
 }
 
