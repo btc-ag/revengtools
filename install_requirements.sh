db1b05741c29585de2acd97a48addc45bb780e2f
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-05-03 12:17:56 +0200

    Fix install_requirements.sh on travis (no user site packages)

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ "$DIST" == "" ] ; then DIST=ubuntu-14.04 ; fi
+if [ "$TRAVIS" == "true" ] ; then
+    INSTALL_USER=""
+    PYTHON_SITE_PACKAGES_DIR=$(python -c "import sys;print sys.path[len(sys.path) - 1]")
+else
+    INSTALL_USER="--user"
+    PYTHON_SITE_PACKAGES_DIR=$(python -m site --user-site)
+fi
+
 # pysvn
 if ! python -c "import pysvn" ; then
     wget "http://pysvn.barrys-emacs.org/source_kits/pysvn-1.9.5.tar.gz"
@@ -7,20 +16,18 @@ if ! python -c "import pysvn" ; then
     cd pysvn-1.9.5/Source
     python setup.py configure --pycxx-dir=../Import/pycxx-7.0.3
     make
-    PYTHON_USER_SITE_PACKAGES_DIR=$(python -m site --user-site)
-    mkdir -p ${PYTHON_USER_SITE_PACKAGES_DIR}/pysvn
-    cp pysvn/__init__.py ${PYTHON_USER_SITE_PACKAGES_DIR}/pysvn
-    cp pysvn/_pysvn*.so ${PYTHON_USER_SITE_PACKAGES_DIR}/pysvn
+    mkdir -p ${PYTHON_SITE_PACKAGES_DIR}/pysvn
+    cp pysvn/__init__.py ${PYTHON_SITE_PACKAGES_DIR}/pysvn
+    cp pysvn/_pysvn*.so ${PYTHON_SITE_PACKAGES_DIR}/pysvn
     cd ../..
     rm -rf pysvn-1.9.5 # remove for testing
 fi
 
 # wxPython
 if ! python -c "import wx" ; then
-    if [ "$DIST" == "" ] ; then DIST=ubuntu-14.04 ; fi
     wget "https://extras.wxpython.org/wxPython4/extras/linux/gtk3/$DIST/wxPython-4.0.1-cp27-cp27mu-linux_x86_64.whl"
-    pip install --user wxPython-4.0.1-cp27-cp27mu-linux_x86_64.whl
+    pip install ${INSTALL_USER} wxPython-4.0.1-cp27-cp27mu-linux_x86_64.whl
 fi
 
 # Normal packages
-pip install --user -r requirements.txt
+pip install ${INSTALL_USER} -r requirements.txt
