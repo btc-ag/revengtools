6ed66c2dd7841dcfc42c58f732ab53745a6d6b6f
Simon Giesecke <simon.giesecke@btc-ag.com>
2018-05-03 16:34:49 -0400

    Fix coverage uploads to codecov

--- run-tests.sh
+++ run-tests.sh
@@ -14,37 +14,42 @@ set -e -x
   TEMP_IGNORE_DOCTESTS="--ignore=commons/config_util.py --ignore=systems/cab/header_linker.py --ignore=systems/cab/dependency_output_experimental.py --ignore=systems/cab/dependency_output.py --ignore=cpp/idep/threaded_cdep_include_deps.py --ignore=configuration/config_base.py --ignore=clustering/clustering_graphviz.py --ignore=base/basic_config_default.py --ignore=cpp/incl_deps/include_list_generator.py --ignore=python/python_util.py --ignore=clustering/clustering.py --ignore=cpp/idep/dirwise_cdep_include_deps.py "
   TEMP_IGNORE_UNITTESTS="--ignore=test/unit_tests/base/diagnostics_util_test.py --ignore=test/unit_tests/infrastructure/ctags/ctag_class_mapper_test.py --ignore=test/unit_tests/cpp/incl_deps/include_list_generator_test.py --ignore=test/unit_tests/cpp/incl_deps/include_resolver_util_test.py"
   TEMP_IGNORE_INTEGRATIONTESTS="--ignore=test/integration_tests/os_util_long_test.py --ignore=test/integration_tests/cabCsharp_tests/cab_csharp_test.py "
-  py.test --doctest-modules --cov=./ --ignore=site-packages --ignore=pydep_run.py --ignore=independent_part_run.py --ignore=infrastructure/graph_layout/zest --ignore=test --verbose $TEMP_IGNORE_DOCTESTS
+  py.test --doctest-modules --cov=./ --cov-report= --ignore=site-packages --ignore=pydep_run.py --ignore=independent_part_run.py --ignore=infrastructure/graph_layout/zest --ignore=test --verbose $TEMP_IGNORE_DOCTESTS
   py.test --doctest-modules --cov=./ --cov-append test/unit_tests $TEMP_IGNORE_UNITTESTS --verbose
   if [ "$TRAVIS" == "true" ] ; then
+    coverage xml -o coverage.xml
     codecov --flags unit
-    coverage erase
   fi
+  coverage erase
 
   # TODO currently all integration tests are ignored, which causes py.test to return 5
   #py.test --doctest-modules --cov=./ --cov-append test/integration_tests $TEMP_IGNORE_INTEGRATIONTESTS --verbose
   #if [ "$TRAVIS" == "true" ] ; then
+  #  coverage xml -o coverage.xml
   #  codecov --flags integration
-  #  coverage erase
   #fi
+  #coverage erase
 
   # TODO currently there are no real tests in test/system_tests
   #py.test --doctest-modules --cov=./ --cov-append test/system_tests --verbose
   # TODO do this with the test framework?
   LOCAL_SOURCE_BASE_DIR=$(pwd) LANGUAGE=python coverage run -a pydep_run.py | dot
+  coverage report
 
   if [ "$TRAVIS" == "true" ] ; then
+    coverage xml -o coverage.xml
     codecov --flags system
-    coverage erase
   fi
+  coverage erase
 
   # TODO currently there are no real tests in test/perf_tests
   #py.test --doctest-modules --cov=./ --cov-append test/perf_tests --verbose
 
   #if [ "$TRAVIS" == "true" ] ; then
+  #  coverage xml -o coverage.xml
   #  codecov --flags perf
-  #  coverage erase
   #fi
+  #coverage erase
 
   exit
 }
